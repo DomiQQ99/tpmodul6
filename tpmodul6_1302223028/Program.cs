95842711e06b5addc87b8ed9eb185ca913c61e8f
Rifqi Alghifari <137023042+DomiQQ99@users.noreply.github.com>
2024-03-25 02:40:18 +0700

    menambahkan class Saya TubeVideo

--- tpmodul6_1302223028/Program.cs
+++ tpmodul6_1302223028/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+internal class Program
+{
+
+    class random
+    {
+        public int idRandom()
+        {
+            Random id = new Random();
+            return id.Next(10000, 99999);
+        }
+    }
+    class sayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playcount;
+
+        public sayaTubeVideo(string title)
+
+        {
+            random random = new random();
+            this.title = title;
+            id = random.idRandom();
+            playcount = 0;
+
+        }
+
+        public void increasePlaycount(int playcount)
+        {
+            this.playcount += playcount;
+
+        }
+        public void printVideoDetail()
+        {
+            Console.WriteLine($"ID :  {this.id}");
+            Console.WriteLine($"JUDUL : {this.title}");
+            Console.WriteLine($"Play Count : {this.playcount} ");
+        }
+
+    }
+    private static void Main(string[] args)
+    {
+
+        sayaTubeVideo video = new sayaTubeVideo("Tutorial Design by contract - Rifqi Alghifari");
+        video.increasePlaycount(2223028);
+        video.printVideoDetail();
+
+    }
+}
--- tpmodul6_1302223028/obj/Debug/net8.0/tpmodul6_1302223028.AssemblyInfo.cs
+++ tpmodul6_1302223028/obj/Debug/net8.0/tpmodul6_1302223028.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul6_1302223028")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a63caab4bc3647d3ebe45ed26dc771a4ccae2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul6_1302223028")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul6_1302223028")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
